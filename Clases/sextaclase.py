3e7fbb0f85e64603bd53baf5cbc7ccd0f66f0a6a
Lauraparedesc <lauraparedescuatin@gmail.com>
2020-08-29 15:54:19 -0500

    sextaclase

---
+++ Clases/sextaclase.py
@@ -0,0 +1,10 @@
+#Ciclos for
+for repeticion in range (3,10):
+    print(repeticion)
+print("¡Chao!")
+
+preguntaNumero= "Por favor ingresa un número para saber hasta donde debo contar : "
+numero = int(input(preguntaNumero))
+
+for i in range (numero):
+    print(i + 1) 
