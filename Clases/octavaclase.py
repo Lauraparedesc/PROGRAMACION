479f20244a5ac3740ff7565ea75900a9894a21f2
Lauraparedesc <lauraparedescuatin@gmail.com>
2020-09-04 13:15:48 -0500

    octavaclase

---
+++ Clases/octavaclase.py
@@ -0,0 +1,17 @@
+import random 
+
+preguntaNumero = "Adivina el número que arrojo el dado : "
+
+mensajeFallido = "Fallaste!, no es el número"
+mensajeExitoso = "Felicitaciones has acertado el número"
+mensajeDespedida = "Gracias por jugar, nos vemos"
+
+dado = random.randint (1,6) 
+numeroIngresado = int(input(preguntaNumero))
+
+while( dado != numeroIngresado) :
+    print(mensajeFallido)
+    numeroIngresado = int(input(preguntaNumero))
+print(mensajeExitoso)
+print(mensajeDespedida)
+
