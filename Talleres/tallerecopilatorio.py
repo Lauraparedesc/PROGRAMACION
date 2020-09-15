69d1af981ba6b270726070c81ae57bdc12939375
Lauraparedesc <lauraparedescuatin@gmail.com>
2020-09-14 21:04:48 -0500

    tallerecopilatorio

---
+++ Talleres/tallerecopilatorio.py
@@ -0,0 +1,14 @@
+listaDolares = [20000,30000,4000,2500,1000,7600]
+preguntaMenu = '''
+Por favor ingrese alguna de estas opciones:
+            1- Convertir dolares 
+            2- Mostrar categoria de ingresos
+            3- Ver máximo, minimo y promedio de ingresos 
+            4- Para salir del programa
+: '''
+preguntaConversionDolares = '''
+Por favor ingrese alguna de estas opciones:
+            C- Convertir a pesos colombianos
+            D- Convertir a dolares
+            E- Convertir a euros
+: '''
