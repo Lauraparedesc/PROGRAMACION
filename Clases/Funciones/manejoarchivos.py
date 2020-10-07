d3aab1829fe05fb6163b3a9d0254ce9e8c76c8f1
Lauraparedesc <lauraparedescuatin@gmail.com>
2020-10-07 12:54:33 -0500

    clase 7/10/20 manejoarchivos

---
+++ Clases/Funciones/manejoarchivos.py
@@ -0,0 +1,27 @@
+#leer
+
+archivo = open ('poema.txt', encoding = 'UTF-8')
+print (archivo)
+lineas = archivo.readlines()
+archivo.close()
+print (lineas)
+listaRenglones = []
+with open ('poema.txt', encoding = 'UTF-8')as lineas:
+    for line in lineas :
+        print (line)
+        listaRenglones.append(line)
+
+print (listaRenglones)
+
+print ('Saludo :\nHola como estás')
+print ('Saludo :\t\t\tHola como estás')
+
+
+nombre = input ('Ingrese su nombre : ')
+edad = int (input('Ingrese su edad : '))
+opinion = input ('Ingrese su opinión : \n')
+
+archivo = open ('opinion.txt','w', encoding = 'UTF-8')
+archivo.write (f'Opinion de {nombre} \n')
+archivo.write (f'Edad : {edad} \n')
+archivo.write (f'Reseña {opinion} \n')
